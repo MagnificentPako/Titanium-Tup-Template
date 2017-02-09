4787621cd84121c84627d9147f83911b2b4f01d4
Paul <paul@niobium.me>
2017-02-09 21:03:30 +0100

    some small changes

--- project/bin/package.lua
+++ project/bin/package.lua
@@ -1,4 +1,41 @@
 --[[
+Titanium License File
+
+At the time of writing, all source code used in Titanium is open source under
+the MIT license.
+
+This essentially means that the code can be used however you wish as long as my
+license (and therefor the license of any work I adapted) is included with my
+code.
+
+Titanium source
+
+All Titanium source is written by Harry Felton unless otherwise stated, the
+below describes what you can do with my code under the conditions given:
+The MIT License (MIT)
+
+Copyright (c) 2016 Harry Felton
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+]]
+
+--[[
     The main method of packaging custom projects that use Titanium.
 
     Call with --help/-h to display help
@@ -308,7 +345,7 @@ do
     for i = 1, #r do
         rI = r[ i ]
         if not ( class_assets[ getName( rI ) ] or extract_assets[ rI ] ) and not ( GLOBAL_EXCLUDE[ rI ] or VFS_EXCLUDE[ rI ] ) then
-            vfs_assets[ rI ] = getFileContents( rI, rI:find("%.lua$") or rI:find("%.ti$") )
+            vfs_assets[ rI:sub(("compiled/"):len()+1, #rI) ] = getFileContents( rI, rI:find("%.lua$") or rI:find("%.ti$") )
         end
     end
 end
